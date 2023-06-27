ccbc752d5a17cc7c7ffc4824994f94ebec56f0e6
Julian-Lop <julian01andres@gmail.com>
2023-06-27 11:05:48 -0500

    comment temp

--- src/classes/index.js
+++ src/classes/index.js
@@ -1,283 +1,283 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
-import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+// import * as THREE from 'three'
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+// import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
+// import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
 
-export class basicCharacterControllerProxy {
-    constructor(animations) {
-        this._animations = animations
-    }
+// export class basicCharacterControllerProxy {
+//     constructor(animations) {
+//         this._animations = animations
+//     }
 
-    get animation() {
-        return this._animations
-    }
-}
+//     get animation() {
+//         return this._animations
+//     }
+// }
 
-export class BasicCharacterController {
-    constructor(params) {
-        this._params = params
-        this._deceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0)
-        this._aceleration = new THREE.Vector3(1, 0.25, 50.0)
-        this._velocity = new THREE.Vector3(0,0,0)
+// export class BasicCharacterController {
+//     constructor(params) {
+//         this._params = params
+//         this._deceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0)
+//         this._aceleration = new THREE.Vector3(1, 0.25, 50.0)
+//         this._velocity = new THREE.Vector3(0,0,0)
 
-        this._animations = {}
-        this._input = new BasicCharacterControllerInput() 
-        this._stateMachine = new CharacterFSM(new basicCharacterControllerProxy(this._animations))
-        this.loadModels()
-    }
+//         this._animations = {}
+//         this._input = new BasicCharacterControllerInput() 
+//         this._stateMachine = new CharacterFSM(new basicCharacterControllerProxy(this._animations))
+//         this.loadModels()
+//     }
 
-    loadModels( ) {
-        // TODO
-    }
-}
+//     loadModels( ) {
+//         // TODO
+//     }
+// }
 
-export class BasicCharacterControllerInput {
-    constructor () {
-        this._keys = {
-            forward : false,
-            backward : false,
-            left : false,
-            right : false,
-            shift : false
-        }
+// export class BasicCharacterControllerInput {
+//     constructor () {
+//         this._keys = {
+//             forward : false,
+//             backward : false,
+//             left : false,
+//             right : false,
+//             shift : false
+//         }
 
-        document.addEventListener('keydown', (e) => this._onKeyDown(e),false)
-        document.addEventListener('keyup', (e) => this._onKeyUp(e),false)
-    }
+//         document.addEventListener('keydown', (e) => this._onKeyDown(e),false)
+//         document.addEventListener('keyup', (e) => this._onKeyUp(e),false)
+//     }
 
-    onKeyDown (e) {
-        switch (e.kyCode) {
-            case 87: //W
-                this._keys.forward = true
-                break;
-            case 65: //A
-                this._keys.left = true
-                break;
-            case 83: //S
-                this._keys.backward = true
-                break;
-            case 68: //D
-                this._keys.right = true
-                break;
-            case 16: // shift ⬆️
-                this._keys.shift = true
-                break;
-            default:
-                break;
-        }
-    }
+//     onKeyDown (e) {
+//         switch (e.kyCode) {
+//             case 87: //W
+//                 this._keys.forward = true
+//                 break;
+//             case 65: //A
+//                 this._keys.left = true
+//                 break;
+//             case 83: //S
+//                 this._keys.backward = true
+//                 break;
+//             case 68: //D
+//                 this._keys.right = true
+//                 break;
+//             case 16: // shift ⬆️
+//                 this._keys.shift = true
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
 
-    onKeyUp (e) {
-        switch (e.kyCode) {
-            case 87: //W
-                this._keys.forward = false
-                break;
-            case 65: //A
-                this._keys.left = false
-                break;
-            case 83: //S
-                this._keys.backward = false
-                break;
-            case 68: //D
-                this._keys.right = false
-                break;
-            case 16: // shift ⬆️
-                this._keys.shift = false
-                break;
-            default:
-                break;
-        }
-    }
-}
+//     onKeyUp (e) {
+//         switch (e.kyCode) {
+//             case 87: //W
+//                 this._keys.forward = false
+//                 break;
+//             case 65: //A
+//                 this._keys.left = false
+//                 break;
+//             case 83: //S
+//                 this._keys.backward = false
+//                 break;
+//             case 68: //D
+//                 this._keys.right = false
+//                 break;
+//             case 16: // shift ⬆️
+//                 this._keys.shift = false
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+// }
 
-export class FiniteStateMachine {
-    constructor () {
-        this._states = {}
-        this._currentState = null
-    }
+// export class FiniteStateMachine {
+//     constructor () {
+//         this._states = {}
+//         this._currentState = null
+//     }
 
-    addState (name, type) {
-        this._states[name] = type
-    }
+//     addState (name, type) {
+//         this._states[name] = type
+//     }
 
-    setState (name) {
-        const prevState = this._currentState
+//     setState (name) {
+//         const prevState = this._currentState
 
-        if(prevState && ((prevState.Name == name)) ) return prevState.Exit()
+//         if(prevState && ((prevState.Name == name)) ) return prevState.Exit()
 
-        const state = new this._states[name](this)
+//         const state = new this._states[name](this)
 
-        this._currentState = state
+//         this._currentState = state
 
-        state.Enter(prevState)
-    }
+//         state.Enter(prevState)
+//     }
 
-    update(timeLapsed, input) {
-        if (this._currentState) {
-            this._currentState.update(timeLapsed, input)
-        }
-    }
-}
+//     update(timeLapsed, input) {
+//         if (this._currentState) {
+//             this._currentState.update(timeLapsed, input)
+//         }
+//     }
+// }
 
-export class CharacterFSM extends FiniteStateMachine {
-    constructor(proxy) {
-        super()
-        this._proxy = proxy
-        this.addState('idle', IdleState)
-        this.addState('walk', WalkState)
-        this.addState('run', RunState)
-    }
-}
+// export class CharacterFSM extends FiniteStateMachine {
+//     constructor(proxy) {
+//         super()
+//         this._proxy = proxy
+//         this.addState('idle', IdleState)
+//         this.addState('walk', WalkState)
+//         this.addState('run', RunState)
+//     }
+// }
 
 
-export class State {
-    constructor(parent) {
-        this._parent = parent
-    }
+// export class State {
+//     constructor(parent) {
+//         this._parent = parent
+//     }
 
-    Enter() {
+//     Enter() {
         
-    }
+//     }
 
-    Update() {
+//     Update() {
         
-    }
+//     }
 
-    Exit() {
+//     Exit() {
         
-    }
-}
+//     }
+// }
 
-export class IdleState extends State {
-    constructor(parent) {
-        super(parent)
-    }
+// export class IdleState extends State {
+//     constructor(parent) {
+//         super(parent)
+//     }
 
-    get Name() {
-        return 'idle'
-    }
+//     get Name() {
+//         return 'idle'
+//     }
 
-    Enter(prevState) {
-        const idleAction = this._parent._proxy.animations['idle'].action
-        if (prevState) {
-            const prevAction = this._parent._proxy.animation[prevState.Name].action
-            idleAction.time = 0.0
-            idleAction.enabled = true
-            idleAction.setEffectiveTimeScale(1.0)
-            idleAction.setEffectiveWeight(1.0)
-            idleAction.crossFadeFrom(prevAction, 0.5, true)
-            idleAction.play()
-        } else {
-            idleAction.play()
-        }
-    }
+//     Enter(prevState) {
+//         const idleAction = this._parent._proxy.animations['idle'].action
+//         if (prevState) {
+//             const prevAction = this._parent._proxy.animation[prevState.Name].action
+//             idleAction.time = 0.0
+//             idleAction.enabled = true
+//             idleAction.setEffectiveTimeScale(1.0)
+//             idleAction.setEffectiveWeight(1.0)
+//             idleAction.crossFadeFrom(prevAction, 0.5, true)
+//             idleAction.play()
+//         } else {
+//             idleAction.play()
+//         }
+//     }
 
-    Update(timeLapsed, input) {
-        if (input._keys.forward || input._keys.backward) {
-            this._parent.setState('walk')
-        }
-    }
+//     Update(timeLapsed, input) {
+//         if (input._keys.forward || input._keys.backward) {
+//             this._parent.setState('walk')
+//         }
+//     }
 
-    Exit() {
+//     Exit() {
 
-    }
-}
+//     }
+// }
 
-export class WalkState extends State {
-    constructor(parent) {
-        super(parent)
-    }
+// export class WalkState extends State {
+//     constructor(parent) {
+//         super(parent)
+//     }
 
-    get Name() {
-        return 'walk'
-    }
+//     get Name() {
+//         return 'walk'
+//     }
 
-    Enter(prevState) {
-        const currentAction = this._parent._proxy._animations['walk'].action
+//     Enter(prevState) {
+//         const currentAction = this._parent._proxy._animations['walk'].action
 
-        if (!prevState) {
-            return currentAction.play()
-        }
-        const prevAction = this._parent._proxy._animations[prevState.Name].action
+//         if (!prevState) {
+//             return currentAction.play()
+//         }
+//         const prevAction = this._parent._proxy._animations[prevState.Name].action
 
-        if (prevState && prevState.Name != 'run') {
-            currentAction.enabled = true
-            currentAction.time = 0.0
-            currentAction.setEffectiveTimeScale(1.0)
-            currentAction.setEffectiveWeight(1.0)
-        }
-        if (prevState && prevState.Name == 'run') {
-            const ratio = currentAction.getClip().duration / prevState.getClip().duration
-            currentAction.time = prevAction.time * ratio
-        }
-        currentAction.crossFadeFrom(prevAction, 0.5, true)
-        currentAction.play()
-    }
+//         if (prevState && prevState.Name != 'run') {
+//             currentAction.enabled = true
+//             currentAction.time = 0.0
+//             currentAction.setEffectiveTimeScale(1.0)
+//             currentAction.setEffectiveWeight(1.0)
+//         }
+//         if (prevState && prevState.Name == 'run') {
+//             const ratio = currentAction.getClip().duration / prevState.getClip().duration
+//             currentAction.time = prevAction.time * ratio
+//         }
+//         currentAction.crossFadeFrom(prevAction, 0.5, true)
+//         currentAction.play()
+//     }
 
-    Update(timeLapsed,input) {
-        if (input._keys.forward || input._keys.backward) {
-            if (input._keys.shift) {
-                this._parent.setState('run')
-            }
-            return 
-        }
+//     Update(timeLapsed,input) {
+//         if (input._keys.forward || input._keys.backward) {
+//             if (input._keys.shift) {
+//                 this._parent.setState('run')
+//             }
+//             return 
+//         }
 
-        this._parent.setState('idle')
-    }
+//         this._parent.setState('idle')
+//     }
 
-    Exit() {
+//     Exit() {
         
-    }
-}
+//     }
+// }
 
-export class RunState extends State {
-    constructor() {
+// export class RunState extends State {
+//     constructor() {
         
-    }
+//     }
 
-    constructor(parent) {
-        super(parent)
-    }
+//     constructor(parent) {
+//         super(parent)
+//     }
 
-    get Name() {
-        return 'run'
-    }
+//     get Name() {
+//         return 'run'
+//     }
 
-    Enter(prevState) {
-        const currentAction = this._parent._proxy._animations['run'].action
-        if (!prevState) {
-            return currentAction.play()
-        }
+//     Enter(prevState) {
+//         const currentAction = this._parent._proxy._animations['run'].action
+//         if (!prevState) {
+//             return currentAction.play()
+//         }
 
-        const prevAction = this._parent._proxy._animations[prevState.Name].action
-        currentAction.enabled = true
+//         const prevAction = this._parent._proxy._animations[prevState.Name].action
+//         currentAction.enabled = true
 
-        if (prevState && prevState.Name !== 'walk') {
-            currentAction.time = 0.0
-            currentAction.setEffectiveTimeScale(1.0)
-            currentAction.setEffectiveWeight(1.0)
-        }
-        if (prevState && prevState.Name == 'walk') {
-            const ratio = currentAction.getClip().duration / prevAction.getClip().duration
-            currentAction.time = prevAction.time * ratio
-        }
+//         if (prevState && prevState.Name !== 'walk') {
+//             currentAction.time = 0.0
+//             currentAction.setEffectiveTimeScale(1.0)
+//             currentAction.setEffectiveWeight(1.0)
+//         }
+//         if (prevState && prevState.Name == 'walk') {
+//             const ratio = currentAction.getClip().duration / prevAction.getClip().duration
+//             currentAction.time = prevAction.time * ratio
+//         }
 
-        currentAction.crossFadeFrom(prevAction, 0.5, true)
-        currentAction.play()
-    }
+//         currentAction.crossFadeFrom(prevAction, 0.5, true)
+//         currentAction.play()
+//     }
 
-    Update(timeLapsed,input) {
-        if (input._keys.forward || input._keys.backward) {
-            if (!input._keys.shift) {
-                this._parent.setState('walk')
-            }
-            return
-        }
-        this._parent.setState('idle')
-    }
+//     Update(timeLapsed,input) {
+//         if (input._keys.forward || input._keys.backward) {
+//             if (!input._keys.shift) {
+//                 this._parent.setState('walk')
+//             }
+//             return
+//         }
+//         this._parent.setState('idle')
+//     }
 
-    Exit() {
+//     Exit() {
         
-    }
-}
+//     }
+// }
